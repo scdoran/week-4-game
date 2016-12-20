bc39ae2ad1f30373197dd984b7521f25cd54861c
Sarah Doran <sarahdoran@wifi_stu-10-38-93-236.uncc.edu>
2016-12-19 21:08:03 -0500

    The game code has been even more simplified.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,25 +5,18 @@ var game = {
 	losses: 0,
 	targetNumber: 0
 }
-// Here are the variables that represent the separate clue images.
-var image = {
-	one: 0,
-	two: 0,
-	three: 0,
-	four: 0
-}
 
 function newGame() {
 // Sets a new random number for the target number.
 	game.targetNumber = Math.floor((Math.random() * 102) + 19);
 	console.log(game.targetNumber);
-// // Sets new numbers for all of the clues at the start of a new game.
-	image.one = Math.floor(Math.random() * 12) + 1;
-	image.two = Math.floor(Math.random() * 12) + 1;
-	image.three = Math.floor(Math.random() * 12) + 1;
-	image.four = Math.floor(Math.random() * 12) + 1;
+
+// Random number function called to make the clueImage ID value set to a random number.
+	randomNumber();
+
 // Sets the game count to 0.
 	game.count = 0; 
+
 //Resets the displays for the target number, game count, wins, losses and the report. 
 	$("#target-number").text(game.targetNumber);
 	$("#user-number").text(game.count);
@@ -32,31 +25,44 @@ function newGame() {
 	setTimeout(function(){$("#report").empty()}, 1000);
 }
 
-$(document).ready(function() {
-    newGame();
-
+// Sets new numbers for all of the clues at the start of a new game.
+function randomNumber() {
+// Applying data attributes to each of the image IDs (random number between 1-12).
 	$("#clueImage1").attr("data-clueValue", Math.floor(Math.random() * 12) + 1);
 	$("#clueImage2").attr("data-clueValue", Math.floor(Math.random() * 12) + 1);
 	$("#clueImage3").attr("data-clueValue", Math.floor(Math.random() * 12) + 1);
 	$("#clueImage4").attr("data-clueValue", Math.floor(Math.random() * 12) + 1);
+}
 
+// This function will run the game when the page is ready.
+$(document).ready(function() {
+
+// Function called to run the game.
+    newGame();
+
+// Random number function called to make the clueImage ID value set to a random number.
+	randomNumber();
+
+// Applying a click event to the clue images by grabbing the class. 
 	$(".clueImage").click(function(event){
 
+// This is adding the value of the clicked picture based on the data-clueValue defined above.
+		game.count += parseInt($(event.target).attr("data-clueValue"));
+
+// This will document the game count.
 		$("#user-number").text(game.count);
 
-		game.count += parseInt($(event.target).attr("data-clueValue"));
-		console.log(event.target);
-
+// If the game.count(score) is equal to the targetNumber the user will win and a new game will start.
 		if (game.count === game.targetNumber) {
 			game.wins++;
 			$("#report").text("You win!");
 			newGame();
-
+// If the game.count(score) is greater than the targetNumber the user will lose and a new game will start.
 		} else if (game.count > game.targetNumber) {
 			game.losses++;
 			$("#report").text("You lose!");
 			newGame();
-
+// If the user has not met or exceeded the targetNumber the game will continue.
 		} else {
 			return;
 		}
